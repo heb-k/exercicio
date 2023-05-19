d1a4df25898f75aaf047583e9fc82bf80dda8b16
heb-k <hebe0465@gmail.com>
2023-05-19 19:07:54 -0300

    firts commit

---
+++ ex2class/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            produto p;
+            p = new produto();
+
+            Console.WriteLine("entre os dados do produto:");
+            Console.Write("nome do produto: ");
+            p.nome = Console.ReadLine();
+            Console.Write("preço do produto: ");
+            p.preco = double.Parse(Console.ReadLine());
+            Console.Write("quantidade do produto: ");
+            p.qtd = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine();
+            Console.WriteLine("dados do produto: "+p);
+
+            Console.WriteLine("digite o numero de  de produtos a ser adicionado ao estoque: ");
+            int qtdA = int.Parse(Console.ReadLine());
+            p.adicionar(qtdA);
+            Console.WriteLine("Dados atualizados: "+p);
+
+            Console.WriteLine("digite o numero de  de produtos a ser retirado do estoque: ");
+            qtdA = int.Parse(Console.ReadLine());
+            p.remover(qtdA);
+            Console.WriteLine("Dados atualizados: " + p);
+
+
+
+        }
+    }
+}
---
+++ ex2class/produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2class
+{
+    class produto
+    {
+        public string nome;
+        public double preco;
+        public int qtd;
+
+        public double valortotal()
+        {
+            return preco * qtd;
+        }
+
+
+        public void adicionar(int quantidade)
+        {
+            qtd = qtd + quantidade;
+        }
+
+        public void remover(int quantidade)
+        {
+            qtd = qtd - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "+nome
+            + ", R$ "
+            + preco.ToString("F2")
+            + ", "
+            + qtd
+            + " unidades, Total: $ "
+            + valortotal().ToString("F2");
+        }
+
+
+
+    }
+}
